a7164d9e7b6abc349190dced114506923a86cf07
Jonathan Morelos Martinez <jmorelos209@gmail.com>
2019-07-18 13:50:16 -0500

    Update database.js
    changes credentials

--- config/database.js
+++ config/database.js
@@ -3,9 +3,9 @@ var mysql 		= require('mysql');
 
 
 var connection = mysql.createConnection({
-  host      : 'axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user      : 'k2lxkuqylygthqt3',
-  password  : 'g9jhfvp87y5d5lqu',
+  host      : 'host',
+  user      : 'user',
+  password  : 'pass',
   port    : '3306'
 });
 
